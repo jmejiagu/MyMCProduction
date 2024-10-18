67222e1995beed5361d982b789014ea33889c2b2
jhovanny <jhovanny.andres.mejia.guisao@cern.ch>
2024-10-18 11:10:07 -0600

    add fragment file for Lambdab 2023

---
+++ HIN/Files2023/LbToJpsiL0_HIN_Run3-2023_HydjetDrumMC_5p36TeV.py
@@ -0,0 +1,133 @@
+#Pythia fragment for filtered B0 -> J/psi(mu+mu-)Kshort(pi+pi-) at 5.02TeV
+#author: jhovanny.andres.mejia.guisao@cern.ch
+#EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/HIN-HINPbPbAutumn18GSHIMix-00005
+#EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/HIN-HiFall15-00043
+#EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/BPH-Run3Summer22EEGS-00107
+#EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/BPH-RunIISummer15GS-00163
+#For 2023
+#EXAMPLE TAKEN FROM https://gitlab.cern.ch/cms-hin-pag/ping-forward-upc/mcrequest/-/tree/master/PYTHIA8_Fragments
+#EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/BPH-Run3Summer23GS-00122 
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+#from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import * ## This was in 2018 silmulation, for 13.0 TeV
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(5020.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            list_forced_decays = cms.vstring('MyLambdab0','MyantiLambdab0'),
+            operates_on_particles = cms.vint32(5122,-5122),    # we care just about our signal particles
+            convertPythiaCodes = cms.untracked.bool(False),
+            user_decay_embedded= cms.vstring(
+"""
+#
+# This is the decay file for the decay B0 -> J/psi(->mumu) Kshort(->pipi)
+#
+Alias      MyLambdab0       Lambda_b0
+Alias      MyantiLambdab0   anti-Lambda_b0
+ChargeConj MyLambdab0       MyantiLambdab0
+Alias      MyLambda         Lambda0
+Alias      MyLambdabar      anti-Lambda0
+ChargeConj MyLambdabar      MyLambda
+Alias      MyJpsi           J/psi
+ChargeConj MyJpsi           MyJpsi
+#
+Decay MyLambdab0
+1.000  MyLambda  MyJpsi  HELAMP 1 0 0.129 -2.523 1.021 1.122 0.145 1.788;
+Enddecay
+Decay MyantiLambdab0
+1.000  MyLambdabar  MyJpsi  HELAMP 1 0 0.129 -2.523 1.021 1.122 0.145 1.788;
+Enddecay
+#
+#
+Decay MyLambda
+1.000   p+  pi-  LAMBDA2PPIFORLAMBDAB2LAMBDAV 0 1; 
+Enddecay
+#
+Decay MyLambdabar
+1.000   anti-p-  pi+  LAMBDA2PPIFORLAMBDAB2LAMBDAV 0 1;
+Enddecay
+#
+Decay MyJpsi
+  1.000         mu+       mu-         PHOTOS VLL;
+Enddecay
+#
+End
+"""
+            ),
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            #'HardQCD:all = on',
+            'HardQCD:gg2bbbar    = on ',
+            'HardQCD:qqbar2bbbar = on ',
+            'HardQCD:hardbbbar   = on',
+            'PhaseSpace:pTHatMin = 2.',
+            ## to improve the statistics of generated events in regions where the number of events would normally be low (example, events with high pT values).
+            #'PhaseSpace:bias2Selection = on',
+            ## This parameter controls the strength of the applied bias. The bias generally takes the form of a weight proportional to a power of pT. The higher the value of bias2SelectionPow,
+            ## the more aggressive the bias in favor of events with high pT. When the value is 2.2, the weight applied to the events will be proportional to pT^{2.2}.
+            #'PhaseSpace:bias2SelectionPow = 2.2',
+            ## Reference point for the bias. which means that events will be biased from a reference pT equal to 1 GeV.
+            ## Events with pT lower than this value will not be as affected by the bias, while those with higher pT values will be more favored.
+            #'PhaseSpace:bias2SelectionRef = 1'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+###### Filters ##########
+
+decayfilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(2),
+    ParticleID      = cms.untracked.int32(5122),
+    DaughterIDs     = cms.untracked.vint32(443, 3122),
+    MinPt           = cms.untracked.vdouble(-1., -1),
+    MinEta          = cms.untracked.vdouble(-9999., -9999.),
+    MaxEta          = cms.untracked.vdouble( 9999., 9999.)
+    )
+
+jpsifilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(2),
+    MotherID        = cms.untracked.int32(5122),
+    ParticleID      = cms.untracked.int32(443),
+    DaughterIDs     = cms.untracked.vint32(13, -13),
+    MinPt           = cms.untracked.vdouble(1.0, 1.0),
+    MinEta          = cms.untracked.vdouble(-2.7, -2.7),
+    MaxEta          = cms.untracked.vdouble( 2.7, 2.7)
+    )
+
+lambdafilter = cms.EDFilter(
+    "PythiaDauVFilter",
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(2),
+    MotherID        = cms.untracked.int32(5122),
+    ParticleID      = cms.untracked.int32(3122),
+    DaughterIDs     = cms.untracked.vint32(2212, -211),
+    MinPt           = cms.untracked.vdouble(0.25, 0.25),
+    MinEta          = cms.untracked.vdouble(-3.0, -3.0),
+    MaxEta          = cms.untracked.vdouble( 3.0, 3.0)
+    )
+
+ProductionFilterSequence = cms.Sequence(generator*decayfilter*jpsifilter*lambdafilter)
---
+++ HIN/Files2023/prepare_Lb-JpsiL0_2023.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+## for 2018 all steps from https://cms-pdmv-prod.web.cern.ch/mcm/requests?dataset_name=BtoPsi2S_pThat-2_TuneCP5-EvtGen_HydjetDrumMB_5p02TeV_pythia8&page=0&shown=127
+## for 2023 I have follow the suggestions by Saray and the oficial twiki: https://twiki.cern.ch/twiki/bin/viewauth/CMS/MCFor2023PbPb5p36TeV
+
+#export SCRAM_ARCH=slc6_amd64_gcc700
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_13_0_20_HeavyIon/src ] ; then
+  echo release CMSSW_13_0_20_HeavyIon already exists
+else
+  scram p CMSSW CMSSW_13_0_20_HeavyIon
+fi
+cd CMSSW_13_0_20_HeavyIon/src
+eval `scram runtime -sh`
+
+# Configuration parameters
+CHANNEL_DECAY="1-BdtoJpsiKs0"
+step0_fragmentfile="BdToJpsiKs0_HIN_Run3-2023_HydjetDrumMC_5p36TeV.py"
+step0_configfile="step0-RAWSIM-${CHANNEL_DECAY}-run_cfg.py"
+step0_resultfile="step0-RAWSIM-${CHANNEL_DECAY}-result.root"
+
+## Download fragment from myGitHub
+curl -s -k https://raw.githubusercontent.com/jmejiagu/MyMCProduction/refs/heads/main/HIN/Files2023/$step0_fragmentfile --retry 3 --create-dirs -o Configuration/GenProduction/python/$step0_fragmentfile
+[ -s Configuration/GenProduction/python/$step0_fragmentfile ] || exit $?;
+
+scram b
+cd ../..
+
+# Maximum validation duration: 57600s
+# Margin for validation duration: 30%
+# Validation duration with margin: 57600 * (1 - 0.30) = 40320s
+# Time per event for each sequence: 1.1030s
+# Threads for each sequence: 1
+# Time per event for single thread for each sequence: 1 * 1.1030s = 1.1030s
+# Which adds up to 1.1030s per event
+# Single core events that fit in validation duration: 40320s / 1.1030s = 36554
+# Produced events limit in McM is 10000
+# According to 0.0140 efficiency, validation should run 10000 / 0.0140 = 712758 events to reach the limit of 10000
+# Take the minimum of 36554 and 712758, but more than 0 -> 36554
+# It is estimated that this validation will produce: 36554 * 0.0140 = 512 events
+EVENTS=1000
+
+## Step 0, GSMix campaign (PYTHIA8 only)
+## cmsdriver comman
+cmsDriver.py  Configuration/GenProduction/python/$step0_fragmentfile --python_filename $step0_configfile --mc --eventcontent RAWSIM --datatier GEN-SIM --fileout file:$step0_resultfile --conditions 130X_mcRun3_2023_realistic_HI_v18 --beamspot MatchHI --step GEN,SIM --scenario HeavyIons --geometry DB:Extended --era Run3_pp_on_PbPb --pileup HiMixGEN --pileup_input "dbs:/MinBias_Drum5F_5p36TeV_hydjet/HINPbPbSpring23GS-130X_mcRun3_2023_realistic_HI_v18-v2/GEN-SIM" --no_exec -n $EVENTS || exit $? ;     
+### --customise Configuration/DataProcessing/Utils.addMonitoring, is this missing????
+
+sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper \nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()" $step0_configfile
+echo cmsDriver for step-0 Gen ok
+
+
+## cmssw release for steps 1, 2, and 3
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_13_2_10/src ] ; then
+  echo release CMSSW_13_2_10 already exists
+else
+  scram p CMSSW CMSSW_13_2_10
+fi
+cd CMSSW_13_2_10/src
+eval `scram runtime -sh`
+
+scram b
+cd ../..
+
+## Step 1, Digi campaign (DIGI-RAW)
+## Configuration parameters
+step1_configfile="step1-PREMIXRAW-${CHANNEL_DECAY}-run_cfg.py"
+step1_resultfile="step1-PREMIXRAW-${CHANNEL_DECAY}-result.root"
+
+## cmsDriver comman
+cmsDriver.py --python_filename $step1_configfile --mc --eventcontent RAWSIM --datatier GEN-SIM-DIGI-RAW --conditions 132X_mcRun3_2023_realistic_HI_v9 --step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:HIon --geometry DB:Extended --era Run3_pp_on_PbPb_2023 --filein file:$step0_resultfile --fileout file:$step1_resultfile  --pileup HiMix --pileup_input "dbs:/MinBias_Drum5F_5p36TeV_hydjet/HINPbPbSpring23GS-130X_mcRun3_2023_realistic_HI_v18-v2/GEN-SIM" --no_exec -n -1 ;  
+
+sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper\nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()" $step1_configfile
+echo cmsDriver for step-1 DIGI ok
+
+## Step 2, Reco campaign (RECO)
+## Configuration parameters 
+step2_configfile="step2-AODSIM-${CHANNEL_DECAY}-run_cfg.py"
+step2_resultfile="step2-AODSIM-${CHANNEL_DECAY}-result.root"
+
+## cmsDriver comman
+cmsDriver.py --python_filename $step2_configfile --mc --eventcontent AODSIM --datatier AODSIM --conditions 132X_mcRun3_2023_realistic_HI_v9 --customise_commands "process.hltSiStripRawToDigi.ProductLabel='rawDataCollector';process.hltScalersRawToDigi.scalersInputTag='rawDataCollector'" --step REPACK:DigiToApproxClusterRaw,RAW2DIGI,L1Reco,RECO --era Run3_pp_on_PbPb_approxSiStripClusters_2023 --filein file:$step1_resultfile --fileout file:$step2_resultfile --no_exec -n -1 ;
+## --runUnscheduled,  is this missing????
+
+sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper\nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()" $step2_configfile
+echo cmsDriver for step-2 RECO ok
+
+
+## step3, MINIAOD campaign
+## Configuration parameters 
+step3_configfile="step3-MINIAODSIM-${CHANNEL_DECAY}-run_cfg.py"
+step3_resultfile="step3-MINIAODSIM-${CHANNEL_DECAY}-result.root"
+
+## cmsDriver command
+cmsDriver.py --python_filename $step3_configfile --mc --eventcontent MINIAODSIM --datatier MINIAODSIM --conditions 132X_mcRun3_2023_realistic_HI_v9 --step PAT --geometry DB:Extended --era Run3_pp_on_PbPb_2023 --filein file:$step2_resultfile --fileout file:$step3_resultfile --no_exec -n -1 ;
+## runUnscheduled,  is this missing????
+## --customise Configuration/DataProcessing/Utils.addMonitoring,  is this missing????      
+
+echo cmsDriver for step-3 MINIAOD ok
+
+
+
+
+
+
+
