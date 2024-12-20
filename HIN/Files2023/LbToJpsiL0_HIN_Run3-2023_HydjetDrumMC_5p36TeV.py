1744acfb7f3aa9ad4f1d1025fbe2a509cda093d7
jhovanny <jhovanny.andres.mejia.guisao@cern.ch>
2024-12-20 11:14:59 -0600

    test lambdab file

--- HIN/Files2023/LbToJpsiL0_HIN_Run3-2023_HydjetDrumMC_5p36TeV.py
+++ HIN/Files2023/LbToJpsiL0_HIN_Run3-2023_HydjetDrumMC_5p36TeV.py
@@ -1,4 +1,4 @@
-#Pythia fragment for filtered B0 -> J/psi(mu+mu-)Kshort(pi+pi-) at 5.36TeV
+#Pythia fragment for filtered Lambdab -> J/psi(->mumu) Lambda(->Ppi) at 5.36TeV
 #author: jhovanny.andres.mejia.guisao@cern.ch
 #For 2018
 #EXAMPLE TAKEN FROM https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/HIN-HINPbPbAutumn18GSHIMix-00005
@@ -30,7 +30,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
             user_decay_embedded= cms.vstring(
 """
 #
-# This is the decay file for the decay B0 -> J/psi(->mumu) Kshort(->pipi)
+# This is the decay file for the decay Lambdab -> J/psi(->mumu) Lambda(->Ppi)
 #
 Alias      MyLambdab0       Lambda_b0
 Alias      MyantiLambdab0   anti-Lambda_b0
@@ -48,19 +48,9 @@ Decay MyantiLambdab0
 1.000  MyLambdabar  MyJpsi  PHSP;
 Enddecay
 #
-#
-Decay MyLambda
-1.000   p+  pi-  PHSP; 
-Enddecay
-#
-Decay MyLambdabar
-1.000   anti-p-  pi+  PHSP;
-Enddecay
-#
 Decay MyJpsi
   1.000         mu+       mu-         PHOTOS VLL;
 Enddecay
-#
 End
 """
             ),
@@ -111,16 +101,4 @@ jpsifilter = cms.EDFilter(
     MaxEta          = cms.untracked.vdouble( 2.7, 2.7)
     )
 
-lambdafilter = cms.EDFilter(
-    "PythiaDauVFilter",
-    verbose         = cms.untracked.int32(1),
-    NumberDaughters = cms.untracked.int32(2),
-    MotherID        = cms.untracked.int32(5122),
-    ParticleID      = cms.untracked.int32(3122),
-    DaughterIDs     = cms.untracked.vint32(2212, -211),
-    MinPt           = cms.untracked.vdouble(0.25, 0.25),
-    MinEta          = cms.untracked.vdouble(-3.0, -3.0),
-    MaxEta          = cms.untracked.vdouble( 3.0, 3.0)
-    )
-
-ProductionFilterSequence = cms.Sequence(generator*decayfilter*jpsifilter*lambdafilter)
+ProductionFilterSequence = cms.Sequence(generator*decayfilter*jpsifilter)
