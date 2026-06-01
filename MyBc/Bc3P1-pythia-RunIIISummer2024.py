9e627e7495855f0a1c406a6acec2a20f4f2a1ac1
jhovanny <jhovanny.andres.mejia.guisao@cern.ch>
2026-06-01 13:59:30 -0600

    add Bc3P1 fragment for Run3

---
+++ MyBc/Bc3P1-pythia-RunIIISummer2024.py
@@ -0,0 +1,146 @@
+#Pythia fragment for filtered Bc3P1 -> Bc(J/psi(mu+mu-)pi)gamma at 13.6TeV.   {Bc(3P1)}
+#author: jhovanny.andres.mejia.guisao@cern.ch 
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(True),
+                         comEnergy = cms.double(13600.),
+                         ExternalDecays = cms.PSet(
+                           EvtGen130 = cms.untracked.PSet(
+                             # decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+                             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2020_NOLONGLIFE.DEC'),
+                             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_Bc_2014.pdl'),
+                             #particle_property_file = cms.FileInPath('evt_Bc_2014.pdl'), ### CHECK ?!
+                             convertPythiaCodes = cms.untracked.bool(False),
+                             user_decay_embedded= cms.vstring(
+
+'Particle B_c+      6.27447 0.00000e+00',
+'Particle B_c*+     6.33207 0.00000e+00',
+'Particle B_c(2S)+  6.75000 0.00000e+00 0.0000000e+00 3 2 0.0000000e+00',
+'Particle B_c*(2S)+ 6.76800 0.00000e+00 0.0000000e+00 3 4 0.0000000e+00',
+'Particle B_c-      6.27447 0.00000e+00',
+'Particle B_c*-     6.33207 0.00000e+00',
+'Particle B_c(2S)-  6.75000 0.00000e+00',
+'Particle B_c*(2S)- 6.76800 0.00000e+00',
+'Particle J/psi     3.09690 9.29000e-05',
+'Particle psi(2S)   3.68610 2.94000e-04',
+'Alias B_c+_SIGNAL B_c+',
+'Alias B_c-_SIGNAL B_c-',
+'Alias MyJ/psi J/psi',
+'Alias MyPsi2S psi(2S)',
+'Alias MyD_s+ D_s+',
+'Alias MyD_s- D_s-',
+'Alias MyD_s*+ D_s*+',
+'Alias MyD_s*- D_s*-',
+'ChargeConj B_c+_SIGNAL B_c-_SIGNAL',
+'ChargeConj B_c*+ B_c*-',
+'ChargeConj B_c(2S)+ B_c(2S)-',
+'ChargeConj B_c*(2S)+ B_c*(2S)-',
+'ChargeConj MyJ/psi MyJ/psi',
+'ChargeConj MyPsi2S MyPsi2S',
+'Decay B_c(2S)+',
+'  0.88 B_c+_SIGNAL gamma  PHSP;',
+'  0.12 B_c*+       gamma  PHSP;',
+'Enddecay',
+'CDecay B_c(2S)-',
+'Decay B_c*(2S)+',
+'  1.0 B_c*+        gamma  PHSP;',
+'Enddecay',
+'CDecay B_c*(2S)-',
+'Decay B_c*+',
+'  1.0 B_c+_SIGNAL  gamma   VSP_PWAVE;',
+'Enddecay',
+'CDecay B_c*-',
+# Updated B_c+ decays with multiple channels?
+'Decay B_c+_SIGNAL',
+'  1.0   MyJ/psi    pi+    SVS;',
+'Enddecay',
+'CDecay B_c-_SIGNAL',
+# J/psi decay
+'Decay MyJ/psi',
+'  1.0   mu+  mu-            PHOTOS  VLL;',
+'Enddecay',
+'End'
+                             ),
+                             operates_on_particles = cms.vint32(100541,100543),
+                            # list_forced_decays = cms.vstring('MyBc+','MyBc-'),
+                           ),
+                           parameterSets = cms.vstring('EvtGen130')
+                         ),
+                         PythiaParameters = cms.PSet(
+                           pythia8CommonSettingsBlock,
+                           pythia8CP5SettingsBlock,
+                           processParameters = cms.vstring( # put below any needed pythia parameter
+#
+            '100541:new = B_c(2S)+ B_c(2S)- 3 3 0 6.75000e+00 0.0000000e+00 0.00000 0.00000 0.0000000e+00',
+            '100541:isResonance = false',
+            '100541:addChannel = 1 0.88 0 541 22',
+            '100541:addChannel = 2 0.12 0 543 22',
+            '100541:mayDecay = off',
+#
+            '100543:new = B_c*(2S)+ B_c*(2S)- 3 3 0 6.76800e+00 0.0000000e+00 0.00000 0.00000 0.0000000e+00',
+            '100543:isResonance = false',
+            '100543:addChannel = 1 1.0 0 543 22',
+            '100543:mayDecay = off',
+#
+            '543:m0 = 6.33207',
+            '543:tau0 = 0.',
+            '543:mayDecay = off',
+#
+            '541:m0 = 6.27447',
+            '541:tau0 = 0.153',
+            '541:mayDecay = off',
+#
+            'ProcessLevel:all = off',
+            'ProcessLevel:resonanceDecays = on'
+#
+                           ),
+                           parameterSets = cms.vstring('pythia8CommonSettings',
+                                                       'pythia8CP5Settings',
+                                                       'processParameters'
+                           )
+                        )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+bc2sgenfilter = cms.EDFilter("PythiaDauVFilter",
+    DaughterIDs = cms.untracked.vint32(22),
+    MaxEta = cms.untracked.vdouble(1.7),
+    MinEta = cms.untracked.vdouble(-1.7),
+    MinPt = cms.untracked.vdouble(0.2),
+    NumberDaughters = cms.untracked.int32(1),
+#    ParticleID = cms.untracked.int32(100543),
+    ParticleID = cms.untracked.int32(100541),
+    verbose = cms.untracked.int32(0)
+)
+
+bcgenfilter = cms.EDFilter("PythiaDauVFilter",
+    DaughterIDs = cms.untracked.vint32(211),
+    MaxEta = cms.untracked.vdouble(2.6),
+    MinEta = cms.untracked.vdouble(-2.6),
+    MinPt = cms.untracked.vdouble(1.0),
+    NumberDaughters = cms.untracked.int32(1),
+    ParticleID = cms.untracked.int32(541),
+    verbose = cms.untracked.int32(0)
+)
+
+jpsifilter = cms.EDFilter("PythiaDauVFilter",
+  verbose         = cms.untracked.int32(0),
+  NumberDaughters = cms.untracked.int32(2),
+  MotherID        = cms.untracked.int32(541),
+  ParticleID      = cms.untracked.int32(443),
+  DaughterIDs     = cms.untracked.vint32(13, -13),
+  MinPt           = cms.untracked.vdouble(2.6, 2.6),
+  MinEta          = cms.untracked.vdouble(-2.6, -2.6),
+  MaxEta          = cms.untracked.vdouble( 2.6,  2.6)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*bc2sgenfilter*bcgenfilter*jpsifilter)
